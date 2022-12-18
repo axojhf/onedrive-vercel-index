39e47d413ac95aededa9fb2c32dcc53171ecfe7a
Axojhf <20625305+axojhf@users.noreply.github.com>
2022-12-18 19:50:48 +0800

    fix

--- config/api.config.js
+++ config/api.config.js
@@ -7,14 +7,11 @@
  * - If you are using a E5 Subscription OneDrive for Business account, the direct links of your files are not the same here.
  *   In which case you would need to change directLinkRegex.
  */
-
-var temp_obfuscatedClientSecret = process.env.OBFUSCATED_CLIENT_SECRET;
-
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: process.env.ACCOUNT_CLIENT_ID,
-  obfuscatedClientSecret: temp_obfuscatedClientSecret,
+  clientId: process.env.ACCOUNT_CLIENT_ID || 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
+  obfuscatedClientSecret: process.env.OBFUSCATED_CLIENT_SECRET || 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
